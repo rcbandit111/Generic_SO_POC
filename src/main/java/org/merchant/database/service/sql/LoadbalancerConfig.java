133ec21b480fc036e4d0b51b7f7f2fabb1ad5448
Plamen Terziev <pterziev@datexcorp.com>
2022-04-27 02:48:27 +0300

    Updated code

--- src/main/java/org/merchant/database/service/sql/LoadbalancerConfig.java
+++ src/main/java/org/merchant/database/service/sql/LoadbalancerConfig.java
@@ -1,29 +1,15 @@
 package org.merchant.database.service.sql;
 
-import feign.Client;
-import feign.httpclient.ApacheHttpClient;
-import org.apache.http.conn.HttpClientConnectionManager;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerClient;
 import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
-import org.springframework.cloud.openfeign.ribbon.CachingSpringLoadBalancerFactory;
-import org.springframework.cloud.openfeign.ribbon.LoadBalancerFeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class LoadbalancerConfig {
+public class LoadbalancerConfig extends RibbonLoadBalancerClient  {
 
-  @Bean
-  public Client client(
-      HttpClientConnectionManager httpClientConnectionManager,
-      CachingSpringLoadBalancerFactory lbClientFactory,
-      SpringClientFactory clientFactory) {
-
-    CloseableHttpClient closeableHttpClient = HttpClients.custom()
-        .setConnectionManager(httpClientConnectionManager)
-        .build();
-    ApacheHttpClient client = new ApacheHttpClient(closeableHttpClient);
-    return new LoadBalancerFeignClient(client, lbClientFactory, clientFactory);
+  public LoadbalancerConfig(SpringClientFactory clientFactory) {
+    super(clientFactory);
   }
+
 }
